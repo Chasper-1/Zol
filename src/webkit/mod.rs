648c76acafa23fd6972c8d036d64353161070121
NK <g7082g@gmail.com>
2026-07-03 12:52:36 +0300

    Сделал отображение редактора

--- src/webkit/mod.rs
+++ src/webkit/mod.rs
@@ -1,10 +1,9 @@
 // src/webkit/mod.rs
-use gtk::Application;
 use gtk::prelude::*;
+use gtk::Application;
 use webkit6::prelude::*;
-use webkit6::{Settings, WebView};
+use webkit6::{WebView, Settings};
 
-// Подключаем отдельный файл редактора
 mod editor;
 
 pub fn build_ui(app: &Application) {
@@ -19,7 +18,12 @@ pub fn build_ui(app: &Application) {
     main_paned.set_position(250);
 
     let sidebar_container = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    
+    // Создаем правый контейнер
     let preview_container = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    // ЗАСТАВЛЯЕМ КОНТЕЙНЕР РАСТЯГИВАТЬСЯ НА ВСЁ ОКНО
+    preview_container.set_vexpand(true);
+    preview_container.set_hexpand(true);
 
     let settings = Settings::new();
     settings.set_enable_html5_database(false);
@@ -30,17 +34,18 @@ pub fn build_ui(app: &Application) {
     settings.set_enable_hyperlink_auditing(false);
     settings.set_enable_dns_prefetching(false);
     settings.set_enable_webgl(false);
-    settings.set_auto_load_images(true);
-    settings.set_enable_javascript(true);
+    settings.set_auto_load_images(true); 
+    settings.set_enable_javascript(true); 
 
     let webview = WebView::new();
     webview.set_settings(&settings);
+    // ЗАСТАВЛЯЕМ WEBVIEW РАСТЯГИВАТЬСЯ ВНУТРИ КОНТЕЙНЕРА
+    webview.set_vexpand(true);
+    webview.set_hexpand(true);
 
     let css_styles = include_str!("render.css");
-
-    // Вызываем функцию из отдельного файла для сборки разметки
     let editor_html = editor::generate_editor_html(css_styles);
-
+    
     webview.load_html(&editor_html, None);
     preview_container.append(&webview);
 
